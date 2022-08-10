aaaccba9bd85b87c65d00428f56645b7ae2cc08f
Dustin Lapierre <dustinlapierre@yahoo.com>
2022-08-10 14:31:57 +0200

    Moved navbar and footer to standard Layout, added tag helper that adds the active tag to nav-links if the route matches

---
+++ ProjectPokemon/Pages/Pokedex.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ProjectPokemon.Pages
+{
+    public class PokedexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
