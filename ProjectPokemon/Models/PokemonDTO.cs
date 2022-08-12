3addaf941576d8930099ac68e0e31d41e8558511
Dustin Lapierre <dustinlapierre@yahoo.com>
2022-08-12 13:00:37 +0200

    Added front-facing Pokemon model for the GUI

---
+++ ProjectPokemon/Models/PokemonDTO.cs
@@ -0,0 +1,28 @@
+﻿namespace ProjectPokemon.Models;
+
+//front facing model
+public class PokemonDTO
+{
+    public string Name { get; set; }
+    public int Id { get; set; }
+    public List<Ability> Abilities { get; set; }
+    public List<Type> Types { get; set; }
+    public int BaseHP { get; set; }
+    public int BaseAttack { get; set; }
+    public int BaseDefense { get; set; }
+    public int BaseSpAttack { get; set; }
+    public int BaseSpDefense { get; set; }
+    public int BaseSpeed { get; set; }
+    public string Img { get; set; }
+}
+
+public class Ability
+{
+    public string Name { get; set; }
+    public string Url { get; set; }
+}
+
+public class Type
+{
+    public string Name { get; set; }
+}
--- ProjectPokemon/Pages/Index.cshtml.cs
+++ ProjectPokemon/Pages/Index.cshtml.cs
@@ -15,22 +15,7 @@ namespace ProjectPokemon.Pages
 
         public void OnGet()
         {
-            var poke = PokeApi.GetPokemon("bruxish").Result;
-            Console.WriteLine(poke.Name);
-            Console.WriteLine(poke.Id);
-            Console.WriteLine(poke.Sprites.Other.OfficialArtwork.FrontDefault);
-            foreach (var a in poke.Abilities)
-            {
-                Console.WriteLine(a.Ability.Name);
-            }
-            foreach (var a in poke.Stats)
-            {
-                Console.WriteLine($"{a.Stat.Name}: {a.BaseStat}");
-            }
-            foreach (var a in poke.Types)
-            {
-                Console.WriteLine($"{a.Type.Name}");
-            }
+
         }
     }
 }
