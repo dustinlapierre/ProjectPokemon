e885221215e511c50b0f2ad4c5d8c56efb00f141
Dustin Lapierre <dustinlapierre@yahoo.com>
2022-08-16 10:28:34 +0200

    Variable naming fix

--- ProjectPokemon/Pages/Pokedex.cshtml.cs
+++ ProjectPokemon/Pages/Pokedex.cshtml.cs
@@ -9,27 +9,27 @@ namespace ProjectPokemon.Pages
     public class PokedexModel : PageModel
     {
         [BindProperty]
-        public PokemonDTO? pokemon { get; set; }
+        public PokemonDTO? Pokemon { get; set; }
         [BindProperty]
-        public List<string>? pokemonNames { get; set; }
+        public List<string>? PokemonNames { get; set; }
 
 
         public async Task OnGet(string search)
         {
             //get list of names, bound to the suggestions for the searchbox
-            pokemonNames = await PokeApi.GetAllPokemonNames();
+            PokemonNames = await PokeApi.GetAllPokemonNames();
 
             //there is no query string, so no need to make the API call
             if (search == null)
                 return;
 
             var pokemonModel = await PokeApi.GetPokemon(search);
-            pokemon = new PokemonDTO();
+            Pokemon = new PokemonDTO();
 
             //manual model mapping, replace with automapper!
-            pokemon.Name = pokemonModel.Name;
-            pokemon.Id = pokemonModel.Id;
-            pokemon.Img = pokemonModel.Sprites.Other.OfficialArtwork.FrontDefault;
+            Pokemon.Name = pokemonModel.Name;
+            Pokemon.Id = pokemonModel.Id;
+            Pokemon.Img = pokemonModel.Sprites.Other.OfficialArtwork.FrontDefault;
 
             foreach (var stat in pokemonModel.Stats)
             {
@@ -38,7 +38,7 @@ namespace ProjectPokemon.Pages
                     Name = stat.Stat.Name,
                     BaseStat = stat.BaseStat
                 };
-                pokemon.Stats.Add(mappedStat);
+                Pokemon.Stats.Add(mappedStat);
             }
 
             foreach (var ability in pokemonModel.Abilities)
@@ -48,11 +48,11 @@ namespace ProjectPokemon.Pages
                     Name = ability.Ability.Name,
                     Url = ability.Ability.Url
                 };
-                pokemon.Abilities.Add(mappedAbility);
+                Pokemon.Abilities.Add(mappedAbility);
             }
 
             foreach (var type in pokemonModel.Types)
-                pokemon.Types.Add(type.Type.Name);
+                Pokemon.Types.Add(type.Type.Name);
         }
     }
 }
